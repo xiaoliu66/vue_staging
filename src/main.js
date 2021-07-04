5c9aa0447c80f0549e11ef9a00807197791f1347
xiaoliu66 <lunhuilk@163.com>
2021-07-04 15:00:37 +0800

    使用vue-cli创建vue项目

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 关闭生产提示
 Vue.config.productionTip = false
 
+/*
+   vue.runtime.common.js 和vue.js有何区别
+    vue.runtime.common.js（项目中用的多）：
+      1.不包含模板解析器，打包后体积小。
+      2.配置项中的不能写template，要用render: h=>h(App)代替
+    vue.js:
+      1.包含解析模板器，打包后体积大。
+      2.配置可以写template
+  
+ */
 new Vue({
   render: h => h(App),
 }).$mount('#app')
