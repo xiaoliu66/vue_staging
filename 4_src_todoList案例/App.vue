4a349fa5a177de24bfbefd559e0bf527a465af40
xiaoliu66 <lunhuilk@163.com>
2021-07-08 20:12:39 +0800

    4_src_todoList案例

---
+++ 4_src_todoList案例/App.vue
@@ -0,0 +1,110 @@
+<template>
+    <div class="todo-container">
+        <div class="todo-wrap">
+            <Header :addTodo="addTodo" />
+            <List :todos="todos" :updateTodo="updateTodo" :deleteTodo="deleteTodo" />
+            <Footer :todo="todos" :deleteAll="deleteAll" :updateAll="updateAll" />
+        </div>
+    </div>
+</template>
+
+<script>
+import Header from "./components/Header";
+import List from "./components/List";
+import Footer from "./components/Footer";
+
+export default {
+    name: "App",
+    data() {
+        return {
+            todos: [
+                { id: "001", name: "抽烟", done: true },
+                { id: "002", name: "喝酒", done: true },
+                { id: "003", name: "烫头", done: false },
+                { id: "004", name: "敲代码", done: false },
+            ],
+        };
+    },
+    methods: {
+        // 添加一个todo
+        addTodo(todoObj) {
+            this.todos.unshift(todoObj);
+        },
+        // 更新一个todo
+        updateTodo(id, done) {
+            this.todos = this.todos.map((todoObj) => {
+                if (todoObj.id === id) {
+                    return { ...todoObj, done };
+                } else {
+                    return todoObj;
+                }
+            });
+        },
+        // 删除一个todo
+        deleteTodo(id) {
+            this.todos = this.todos.filter((todoObj) => {
+                return todoObj.id !== id;
+            });
+        },
+        // 全选或全不选
+        updateAll(done) {
+            this.todos = this.todos.map((todo) => {
+                return { ...todo, done };
+            });
+        },
+        // 全部删除
+        deleteAll() {
+            this.todos = this.todos.filter((todoObj) => {
+                return todoObj.done === false;
+            });
+        },
+    },
+    // 注册组件--局部注册
+    components: { Header, List, Footer },
+};
+</script>
+
+<style>
+/*base*/
+body {
+    background: #fff;
+}
+
+.btn {
+    display: inline-block;
+    padding: 4px 12px;
+    margin-bottom: 0;
+    font-size: 14px;
+    line-height: 20px;
+    text-align: center;
+    vertical-align: middle;
+    cursor: pointer;
+    box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
+    border-radius: 4px;
+}
+
+.btn-danger {
+    color: #fff;
+    background-color: #da4f49;
+    border: 1px solid #bd362f;
+}
+
+.btn-danger:hover {
+    color: #fff;
+    background-color: #bd362f;
+}
+
+.btn:focus {
+    outline: none;
+}
+
+.todo-container {
+    width: 600px;
+    margin: 0 auto;
+}
+.todo-container .todo-wrap {
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+}
+</style>
---
+++ 4_src_todoList案例/components/Item.vue
@@ -0,0 +1,82 @@
+<template>
+    <li @mouseenter="updateIsEnter(true)" @mouseleave="updateIsEnter(false)" :class="{ 'high-light': isEnter }">
+        <label>
+            <input :checked="todo.done" type="checkbox" @click="update(todo.id, $event)" />
+            <span>{{ todo.name }}</span>
+        </label>
+        <button class="btn btn-danger" :style="{ display: isEnter ? 'block' : 'none' }" @click="deleteItem(todo.id)">
+            删除
+        </button>
+    </li>
+</template>
+
+<script>
+export default {
+    name: "Item",
+    props: ["todo", "updateTodo", "deleteTodo"],
+    data() {
+        return {
+            isEnter: false,
+        };
+    },
+    methods: {
+        updateIsEnter(flag) {
+            // console.log(flag);
+            this.isEnter = flag;
+        },
+        update(id, event) {
+            // console.log(id,event.target.checked);
+            // 通知App在data中去更新一个todo的值
+            this.updateTodo(id, event.target.checked);
+        },
+        deleteItem(id) {
+            // console.log(id);
+            // 通知App在data中删除一个todo的值
+            if (confirm("确定删除吗？")) {
+                this.deleteTodo(id);
+            }
+        },
+    },
+};
+</script>
+
+<style scoped>
+/*item*/
+li {
+    list-style: none;
+    height: 36px;
+    line-height: 36px;
+    padding: 0 5px;
+    border-bottom: 1px solid #ddd;
+}
+
+li label {
+    float: left;
+    cursor: pointer;
+}
+
+li label li input {
+    vertical-align: middle;
+    margin-right: 6px;
+    position: relative;
+    top: -1px;
+}
+
+li button {
+    float: right;
+    display: none;
+    margin-top: 3px;
+}
+
+li:before {
+    content: initial;
+}
+
+li:last-child {
+    border-bottom: none;
+}
+
+.high-light {
+    background-color: #ddd;
+}
+</style>
